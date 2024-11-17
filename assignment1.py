8ed59e83f238aab852caf4edd14d4cffa7bc98ab
kartik <k62@myseneca.ca>
2024-11-16 19:06:36 -0500

    Completed the TestAfter, TestBefore, TestLeap and TestMonMax function

--- assignment1.py
+++ assignment1.py
@@ -4,98 +4,72 @@
 OPS445 Assignment 1
 Program: assignment1.py 
 The python code in this file is original work written by
-"Student Name". No code in this file is copied from any other source
+Kartik. No code in this file is copied from any other source
 except those provided by the course instructor, including any person,
 textbook, or on-line resource. I have not shared this python script
 with anyone or anything except for submission for grading. I understand
 that the Academic Honesty Policy will be enforced and
 violators will be reported and appropriate action will be taken.
 
-Author: <Student Name>
-Semester: <Fall/Winter/Summer> <Year>
-Description: <fill this in>
+Author: Kartik
+Semester: Fall 2024
+Description: Implements functionality to calculate next and previous days, as well as leap year checks and month limits.
 '''
 
 import sys
 
-def day_of_week(date: str) -> str:
-    "Based on the algorithm by Tomohiko Sakamoto"
-    day, month, year = (int(x) for x in date.split('/'))
-    days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'] 
-    offset = {1:0, 2:3, 3:2, 4:5, 5:0, 6:3, 7:5, 8:1, 9:4, 10:6, 11:2, 12:4}
-    if month < 3:
-        year -= 1
-    num = (year + year//4 - year//100 + year//400 + offset[month] + day) % 7
-    return days[num]
-
 def leap_year(year: int) -> bool:
-    "return true if the year is a leap year"
-    ...
+    """
+    Check whether the following year is a leap year or not
+    """
+    return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
 
-def mon_max(month:int, year:int) -> int:
-    "returns the maximum day for a given month. Includes leap year check"
-    ...
+def mon_max(month: int, year: int) -> int:
+    """
+    The following will return the maximum number of days in a given month.
+    """
+    month_days = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30,
+                  7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
+    if month == 2 and leap_year(year):
+        return 29
+    return month_days[month]
 
-def after(date: str) -> str: 
-    '''
-    after() -> date for next day in DD/MM/YYYY string format
+def after(date: str) -> str:
+    """
+    this function will return the date for the next day in DD/MM/YYYY format.
+    """
+    day, month, year = (int(x) for x in date.split('/'))
+    day += 1
 
-    Return the date for the next day of the given date in DD/MM/YYYY format.
-    This function has been tested to work for year after 1582
-    '''
-    day, mon, year = (int(x) for x in date.split('/'))
-    day += 1  # next day
+    if day > mon_max(month, year):
+        day = 1
+        month += 1
 
-    lyear = year % 4
-    if lyear == 0:
-        leap_flag = True
-    else:
-        leap_flag = False  # this is not a leap year
+    if month > 12:
+        month = 1
+        year += 1
 
-    lyear = year % 100
-    if lyear == 0:
-        leap_flag = False  # this is not a leap year
-
-    lyear = year % 400
-    if lyear == 0:
-        leap_flag = True  # this is a leap year
-    
-    mon_dict= {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30,
-           7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
-    if mon == 2 and leap_flag:
-        mon_max = 29
-    else:
-        mon_max = mon_dict[mon]
-    
-    if day > mon_max:
-        mon += 1
-        if mon > 12:
-            year += 1
-            mon = 1
-        day = 1  # if tmp_day > this month's max, reset to 1 
-    return f"{day:02}/{mon:02}/{year}"
+    return f"{day:02}/{month:02}/{year}"
 
 def before(date: str) -> str:
-    "Returns previous day's date as DD/MM/YYYY"
-    ...
+    """
+    In DD/MM/YYYY format return the date for the previous day
+    """
+    day, month, year = map(int, date.split('/'))
+    day -= 1
 
-def usage():
-    "Print a usage message to the user"
-    print("Usage: " + str(sys.argv[0]) + " DD/MM/YYYY NN")
-    sys.exit()
+    if day == 0:
+        month -= 1
+        if month == 0:
+            month = 12
+            year -= 1
+        day = mon_max(month, year)
 
-def valid_date(date: str) -> bool:
-    "check validity of date"
-    ...
-
-def day_iter(start_date: str, num: int) -> str:
-    "iterates from start date by num to return end date in DD/MM/YYYY"
-    ...
+    return f"{day:02}/{month:02}/{year}"
 
 if __name__ == "__main__":
-    # check length of arguments
-    # check first arg is a valid date
-    # check that second arg is a valid number (+/-)
-    # call day_iter function to get end date, save to x
-    # print(f'The end date is {day_of_week(x)}, {x}.')
-    pass
+    # Tests for Milestone 1 functions
+    print(after("28/02/2020"))  # Expect "29/02/2020" (Leap Year)
+    print(before("01/03/2020")) # Expect "29/02/2020" (Leap Year)
+    print(mon_max(2, 2020))     # Expect 29 (Leap Year)
+    print(mon_max(2, 2021))     # Expect 28 (Non-Leap Year)
